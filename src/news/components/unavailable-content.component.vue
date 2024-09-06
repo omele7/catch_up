60aa1e990a61391630d13ff92109a6b52ea2246d
Diego <dicabrerabuitron@gmail.com>
2024-09-06 16:51:57 -0500

    feat(news): add unavailable content component.

---
+++ src/news/components/unavailable-content.component.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: "unavailable-content",
+  props:{ errors: null}
+}
+
+</script>
+
+<!-- $t es el metodo de traduccion para Vue -->
+<template>
+  <div><h4>{{$t('unavailable-news')}}</h4></div>
+  <div v-for = "error in errors" :key = "error">
+    <h6>{{error}}</h6>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
